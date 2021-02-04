35fcd7ab7fbc2ea73dded4043032aa7a4da9c6a3
woghd9072 <woghd9072@gmail.com>
2021-02-04 17:14:37 +0900

    Add Controller & FeignClient

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,6 +3,9 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @EnableFeignClients
 @SpringBootApplication
@@ -10,4 +13,18 @@ class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
+}
+
+@FeignClient(name = "feign-client", url = "http://localhost:8081")
+interface Client {
+	@GetMapping("/test")
+	fun test(): String
+}
+
+@RestController
+class Controller(private val client: Client) {
+	@GetMapping
+	fun testFeign(): String {
+		return client.test()
+	}
 }
